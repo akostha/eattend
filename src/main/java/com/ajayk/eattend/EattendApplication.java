81ac3ff116be0197bb4d8515617fbffe6d4ec3df
akostha <31945149+akostha@users.noreply.github.com>
2024-03-28 12:29:50 +0530

    Adding initial files into the project

---
+++ src/main/java/com/ajayk/eattend/EattendApplication.java
@@ -0,0 +1,13 @@
+package com.ajayk.eattend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EattendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EattendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajayk/eattend/EattendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajayk.eattend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EattendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

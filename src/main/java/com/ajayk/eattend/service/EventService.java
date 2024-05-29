f0f03a1ee16dc77808ade4981b497711744fd921
Ajay Kostha <akostha@gmail.com>
2024-05-29 21:54:13 +0530

    Populate create timestamp

--- src/main/java/com/ajayk/eattend/service/EventService.java
+++ src/main/java/com/ajayk/eattend/service/EventService.java
@@ -3,6 +3,7 @@ package com.ajayk.eattend.service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,7 @@ public class EventService {
 	}
 
 	public Event saveEvent(Event event) {
+		event.setCreatedAt(LocalDateTime.now());
 		return eventRepository.save(event);
 	}
 	
